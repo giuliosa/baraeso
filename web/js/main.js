3cc9575f061b6b8abda9d708f7ca681d8eb61023
Giulio Caetano Pires de Sá <giuliocaetano@yahoo.com.br>
2017-09-23 15:59:07 -0300

    adicao do dashboard

--- web/js/main.js
+++ web/js/main.js
@@ -1,66 +1,22 @@
+// Mudar tab ao click
+$('[data-group]').each(function () {
+  var $allTarget = $(this).find('[data-target]'),
+    $allClick = $(this).find('[data-click]'),
+    activeClass = 'active';
 
+  $allTarget.first().addClass(activeClass);
+  $allClick.first().addClass(activeClass);
 
-$(function(){
- var nav = $('.header-menu');
- $(window).scroll(function () {
-   if ($(this).scrollTop() > $('.header-menu').innerHeight()) {
-     nav.addClass("menu-fixo");
+  $allClick.click(function (e) {
+    e.preventDefault();
 
-   } else {
-     nav.removeClass("menu-fixo");
-   }
- });
-});
+    var id = $(this).data('click'),
+      $target = $('[data-target="' + id + '"]');
 
+    $allClick.removeClass(activeClass);
+    $allTarget.removeClass(activeClass);
 
-
-/*Identificar o clique no menu
-  Verificar o item que foi clicado e fazer a referência com o alvo
-  Verificar a distância entre o alvo e o topo
-  Animar o scrool até o alvo*/
-
-$('.header-menu ul li a').click(function(e){
-  e.preventDefault();
-
-  var id = $(this).attr('href'),
-      targetOffset = $(id).offset().top,
-      menuHeight = $('.header-menu').innerHeight();
-
-  $('html, body').animate({
-    scrollTop: targetOffset - menuHeight
-  }, 500);
-});
-
-
-$('.drop ul a').click(function(e){
-  e.preventDefault();
-
-  var id = $(this).attr('href'),
-      targetOffset = $(id).offset().top,
-      menuHeight = $('.drop').innerHeight();
-
-  $('html, body').animate({
-    scrollTop: targetOffset - menuHeight
-  }, 500);
-  $('.drop').hide();
-  $('.cross').hide();
-  $('.hamburguer').show();
-});
-
-
-//Hamburguer Menu Dropdown
-$('.cross').hide();
-$('.drop').hide();
-$('.hamburguer').click(function(){
-  $('.drop').slideToggle("slow", function(){
-    $('.hamburguer').hide();
-    $('.cross').show();
-  });
-});
-
-$('.cross').click(function(){
-  $('.drop').slideToggle("slow", function(){
-    $('.cross').hide();
-    $('.hamburguer').show();
+    $target.addClass(activeClass);
+    $(this).addClass(activeClass);
   });
 });
